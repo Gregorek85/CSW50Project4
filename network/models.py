fbc1221220e35303316bfef006ea1dab833c08b4
Gregorek85 <grerad@gmail.com>
2022-06-27 23:40:11 +0200

    models.py created/edited

--- network/models.py
+++ network/models.py
@@ -3,4 +3,29 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    followed_by = models.ManyToManyField(
+        "self", related_name="follows", symmetrical=False, default=None, blank=True
+    )
+
+    @property
+    def number_of_posts(self):
+        return self.posts.count()
+
+    @property
+    def no_of_following(self):
+        return self.follows.count()
+
+    @property
+    def number_of_followers(self):
+        return self.followed_by.count()
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="posts"
+    )
+    content = models.TextField()
+    # AutoFields:
+    publication_datetime = models.DateTimeField(auto_now_add=True)
+    modification_datetime = models.DateTimeField(auto_now=True)
+    liked_by = models.ManyToManyField(User, related_name="likes", blank=True)
