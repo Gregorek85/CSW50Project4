3c234d034c82fb24216a3b54546b8a8add48e046
Gregorek85 <grerad@gmail.com>
2022-06-28 15:45:15 +0200

    added profile page

--- network/views.py
+++ network/views.py
@@ -7,6 +7,10 @@ from django.urls import reverse
 from .models import User
 
 
+def profile(request, user_pk):
+    return render(request, "network/profile.html")
+
+
 def index(request):
     return render(request, "network/index.html")
 
@@ -24,9 +28,11 @@ def login_view(request):
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
         else:
-            return render(request, "network/login.html", {
-                "message": "Invalid username and/or password."
-            })
+            return render(
+                request,
+                "network/login.html",
+                {"message": "Invalid username and/or password."},
+            )
     else:
         return render(request, "network/login.html")
 
@@ -45,18 +51,18 @@ def register(request):
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
-            return render(request, "network/register.html", {
-                "message": "Passwords must match."
-            })
+            return render(
+                request, "network/register.html", {"message": "Passwords must match."}
+            )
 
         # Attempt to create new user
         try:
             user = User.objects.create_user(username, email, password)
             user.save()
         except IntegrityError:
-            return render(request, "network/register.html", {
-                "message": "Username already taken."
-            })
+            return render(
+                request, "network/register.html", {"message": "Username already taken."}
+            )
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
     else:
